961d1edcb2d8b0c8b8e4fd33872419d4c861d9fc
isalim079 <salimreza1025@gmail.com>
2024-01-01 11:51:33 +0600

    authSlice added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
+import { useSelector } from "react-redux";
 
 function App() {
+
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
+
   return (
     <div className="App">
-      <Auth />
-      {/* <Layout /> */}
+      {/* <Auth /> */}
+      <Layout />
     </div>
   );
 }
